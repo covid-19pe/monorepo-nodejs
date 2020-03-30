cceb8f55724602a533811a1c90b6e36d47e4c32c
Giovanni Alexander <alexander.hh.cc@gmail.com>
2020-03-30 01:06:42 -0500

    [add] Validation of dni

--- src/resources/handler.js
+++ src/resources/handler.js
@@ -1,70 +1,89 @@
-"use strict";
+'use strict';
 
-const axios = require("axios");
-const https = require("https");
+const axios = require('axios');
+const https = require('https');
+const Joi = require('@hapi/joi');
 
-module.exports.verifiedDni = async event => {
+const verifiedDniSchema = Joi.object({
+  dni: Joi.string()
+    .required()
+    .regex(/^[0-9]{8}$/)
+    .error(new Error('Wrong DNI')),
+  verificationCode: Joi.string()
+    .required()
+    .regex(/^[0-9]{1}$/)
+    .error(new Error('Wrong verification code')),
+});
 
-    const payload = JSON.parse(event.body);
-    const token = process.env.API_DNI_TOKEN;
-    const endPointApi = process.env.API_DNI_URL;
-    ;
-    const filterKeys = ["dni", "verificationCode"];
+module.exports.verifiedDni = async (event) => {
+  const payload = JSON.parse(event.body);
+  const validation = verifiedDniSchema.validate(payload);
 
-    let normalizeResponse = null;
-    let success = true;
-
-    const instance = axios.create({
-        httpsAgent: new https.Agent({
-            rejectUnauthorized: false
-        })
-    });
-
-    console.log(token);
-    try {
-        const { data } = await instance.get(
-            `${endPointApi}/${payload.dni}?token=${token}`
-        );
-
-        console.log(data)
-
-        normalizeResponse = {
-            dni: data.dni,
-            verificationCode: data.codVerifica,
-            names: data.nombres
-        };
-    } catch (error) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({
-                error: {
-                    code: "WrongDataError",
-                    message: "Input valid dni or verification code"
-                }
-            })
-        };
-    }
-
-    filterKeys.forEach(key => {
-        if (parseInt(payload[key]) !== parseInt(normalizeResponse[key])) {
-            success = false;
-        }
-    });
-
-    if (!success) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({
-                error: {
-                    code: "WrongDataError",
-                    message: "Input valid dni or verification code"
-                }
-            })
-        };
-    }
-
+  if (validation.error) {
     return {
-        statusCode: 200,
-        body: JSON.stringify({ data: normalizeResponse })
+      statusCode: 400,
+      body: JSON.stringify({
+        error: {
+          code: 'ValidationError',
+          message: validation.error.message,
+        },
+      }),
     };
+  }
+
+  const token = process.env.API_DNI_TOKEN;
+  const endPointApi = process.env.API_DNI_URL;
+  const filterKeys = ['dni', 'verificationCode'];
+
+  let normalizeResponse = null;
+  let success = true;
+
+  const instance = axios.create({
+    httpsAgent: new https.Agent({
+      rejectUnauthorized: false,
+    }),
+  });
+
+  try {
+    const { data } = await instance.get(`${endPointApi}/${payload.dni}?token=${token}`);
+
+    normalizeResponse = {
+      dni: data.dni,
+      verificationCode: data.codVerifica,
+      names: data.nombres,
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        error: {
+          code: 'WrongDataError',
+          message: 'Input valid dni or verification code',
+        },
+      }),
+    };
+  }
+
+  filterKeys.forEach((key) => {
+    if (parseInt(payload[key]) !== parseInt(normalizeResponse[key])) {
+      success = false;
+    }
+  });
+
+  if (!success) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        error: {
+          code: 'WrongDataError',
+          message: 'Input valid dni or verification code',
+        },
+      }),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ data: normalizeResponse }),
+  };
 };
